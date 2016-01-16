12a7ffb955665a667b9d0650eb0da9560bf7ed89
Nils Langner <contact@phmlabs.com>
2016-01-16 11:14:49 +0100

    Increased version number

--- bin/KoalaBatch.php
+++ bin/KoalaBatch.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__ . "/../vendor/autoload.php";
 
-define('KOALA_BATCH_VERSION_NUMBER', '0.0.1');
+define('KOALA_BATCH_VERSION_NUMBER', '0.2.0');
 
 $app = new \Koalamon\KoalaBatch\Cli\Application();
 $app->run();
